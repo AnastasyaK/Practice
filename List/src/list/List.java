250a9e7e1ae7218413b13ea32bd65db4112b77b5
AnastasyaK <programmer1310@gmail.com>
2015-06-23 22:56:32 +0300

    rename

--- List/src/list/List.java
+++ List/src/list/List.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package lisp;
+package list;
 
 /**
  *
  * @author Анастасия
  */
-public class Lisp {
+public class List {
 
     /**
      * @param args the command line arguments
